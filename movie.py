940ffd1f00494029e01e233b54bea2d84af31af8
Sheel Patel <86443652+spate1997@users.noreply.github.com>
2024-08-17 14:30:22 -0400

    Update movie.py

--- movie.py
+++ movie.py
@@ -5,3 +5,10 @@ from sklearn.metrics.pairwise import cosine_similarity
 ratings = pd.read_csv('ratings.csv')
 movies = pd.read_csv('movies.csv')
 data = pd.merge(ratings, movies, on='movieId')
+
+# User-Item Matrix
+user_movie_matrix = data.pivot_table(index='userId', columns='title', values='rating')
+
+# Calculate Similarity
+user_similarity = cosine_similarity(user_movie_matrix.fillna(0))
+user_similarity_df = pd.DataFrame(user_similarity, index=user_movie_matrix.index, columns=user_movie_matrix.index)
